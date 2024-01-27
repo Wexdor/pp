79e4a94b28d7a9aa4de9dab1ef996df62a57e561
Wexdor <135797404+Wexdor@users.noreply.github.com>
2024-01-27 17:15:27 +0700

    Update pingpong.py
    No bugs! Final version of Ping Pong

--- pingpong.py
+++ pingpong.py
@@ -37,6 +37,18 @@ game = True
 finish = False
 clock = time.Clock()
 
+racket1 = Player('images.png', 30, 200, 4, 50, 150)
+racket2 = Player('images.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tennis.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE', True, (180,0,0))
+lose2 = font.render('PLAYER 2 LOSE', True, (180,0,0))
+
+speed_x = 3
+speed_y = 3
+
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -44,6 +56,30 @@ while game:
 
     if finish != True:
         window.fill(background)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= -1
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200,200))
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200,200))
+
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
 
     display.update()
     clock.tick(60)
